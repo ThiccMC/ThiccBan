0ade5f9b985f798313de41f8a8278fe004504dd4
Tỷ Nguyễn <uguest17@gmail.com>
2022-11-27 22:14:28 +0700

    fix mobile navigation bar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -9,8 +8,8 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
-      <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"/>
-      <Script src="/js/bootstrap.min.js" />
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"/>
+      <script src="/js/bootstrap.min.js" />
     </Html>
   )
 }
