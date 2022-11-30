77911f5ecec7827e2abd1e235d6004ab12840a75
Tỷ Nguyễn <uguest17@gmail.com>
2022-11-30 21:45:36 +0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,8 +8,8 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
-      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"/>
-      <script src="/js/bootstrap.min.js" />
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" async />
+      <script src="/js/bootstrap.min.js" async />
     </Html>
   )
 }
