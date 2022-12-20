d757fa4e06dbf242f4cbc523bf83fb2417e277ec
Tỷ Nguyễn <uguest17@gmail.com>
2022-12-20 21:51:12 +0700

    fix font issue

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" />
+      </Head>
       <body>
         <Main />
         <NextScript />
